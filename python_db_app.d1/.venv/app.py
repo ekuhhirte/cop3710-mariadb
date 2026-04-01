0c55bc8613a9b7ad8c4100e4e180cf3b04f8bf0f
ekuhhirte <ekuhhirte0836@floridapoly.edu>
2026-04-01 13:30:04 +0000

    Saving some new changes

---
+++ python_db_app.d1/.venv/app.py
@@ -0,0 +1,20 @@
+#app.py
+import os
+os.putenv('LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN', '1')
+import mysql.connector
+from dotenv import load_dotenv
+load_dotenv()
+
+
+print("hello world")
+try:
+    connection = mysql.connector.connect(
+    host=os.getenv('DB_HOST'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    database=os.getenv('DB_NAME'))
+except mysql.connector.Error asA err:
+    print(f"Error: {err}")
+else:
+    print("Connection successful!")
+    connection.close()
